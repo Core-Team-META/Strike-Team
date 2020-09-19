299904cb6bf8c2c68af625e2ba58fe566923a3c1
BuckmonsterCORE <kbuckmaster@manticoregames.com>
2020-09-19 07:57:45 -0700

    Initial Commit

---
+++ Data/Scripts/README.lua
@@ -0,0 +1,12 @@
+﻿--[[
+
+META Sniper Alley Remix
+
+- Buckmonster
+- Keppu
+- Coderz
+
+Remixing Sniper Alley to have capture point game play and more functionality
+
+
+]]--
---
+++ Meta.pbt
@@ -0,0 +1,5 @@
+﻿GameId: "34253646444D2E3604C92F9573041504"
+DisplayName: "META SA Remix"
+GameSerializationVersion: 63
+Framework: "Empty Project"
+StorageVersion: 1
