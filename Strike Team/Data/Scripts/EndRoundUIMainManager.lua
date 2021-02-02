8803faeb12413aeb3297e3c017c68fc672d559f1
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2021-02-02 14:09:13 -0800

    Fixed victory screen player orientation for player 1, game mode name updates on end screen.

--- Strike Team/Data/Scripts/EndRoundUIMainManager.lua
+++ Strike Team/Data/Scripts/EndRoundUIMainManager.lua
@@ -12,6 +12,9 @@ local endRoundManagerRoot = script:GetCustomProperty("EndRoundUIManager"):WaitFo
 
 local victoryEvent = endRoundManagerRoot:GetCustomProperty("UpdateWinnersEvent")
 
+local startTime = os.time()
+
+print("time: " .. tostring(startTime))
 
 function ResetWinningTeam()
 
@@ -22,16 +25,44 @@ end
 function SetWinningTeam(winner)
 	
 	script:SetNetworkedCustomProperty("WinningTeam",winner)
+	
+	script:SetNetworkedCustomProperty("MatchTime","")
+	
+	startTime = os.time()
+
+
+end
+
+function SetRoundLength()
+	
+	local totalTime = os.time() - startTime
+	
+	print(os.time() - startTime)
+	
+	if totalTime <= 0 then
+	
+    	script:SetNetworkedCustomProperty("MatchTime","00:00")
+    	
+  	else
+  	
+	    local minutes = tostring(floor(mod(totalTime,3600)/60))
+  		local seconds = tostring(floor(mod(totalTime,60)))
+  		
+	    script:SetNetworkedCustomProperty("MatchTime",minutes .. ":" .. seconds)
+	    
+  	end
 
 end
 
 function OnGameStateChanged(oldState, newState, hasDuration, time)
-
-	print(newState)
     
     if newState == ABGS.GAME_STATE_ROUND and oldState ~= ABGS.GAME_STATE_ROUND then
     
     	ResetWinningTeam()
+    	
+    elseif newState == ABGS.GAME_STATE_ROUND_END and oldState ~= ABGS.GAME_STATE_ROUND_END then
+    
+    	SetRoundLength()
                 
     end
 end
