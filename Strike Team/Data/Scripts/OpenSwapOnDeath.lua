1ef613c662445549e77e1ba40e2d3a402475ed36
blaking707 <46813060+blaking707@users.noreply.github.com>
2021-02-27 05:08:57 +1100

    Added delay to swap,. Temp Change WeaponAimscopeClient

--- Strike Team/Data/Scripts/OpenSwapOnDeath.lua
+++ Strike Team/Data/Scripts/OpenSwapOnDeath.lua
@@ -1,6 +1,7 @@
 ﻿
 Events.Connect("LivingStateChange",function(state)
     if (state) then    
+        Task.Wait(3)
         Events.Broadcast("SwapPanelForceOpen")
         --Events.Broadcast("ScoreBoardForceOpen")
         Game.GetLocalPlayer().clientUserData.PrevouslySelected = 1
