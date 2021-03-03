746a7ab7f1df9e27e7dee47e99b6f230c6c9cf70
Brent Dunn <brent075@gmail.com>
2021-03-03 12:07:41 -0800

    Added: Faster Reload Perk

--- Strike Team/Data/Scripts/WeaponReloadCastBar_Client.lua
+++ Strike Team/Data/Scripts/WeaponReloadCastBar_Client.lua
@@ -2,7 +2,7 @@ local LOCAL_PLAYER = Game.GetLocalPlayer()
 local PROGRESS_BAR = script:GetCustomProperty("UIProgressBar"):WaitForObject()
 local PARENT_PANEL = script:GetCustomProperty("UIPanel"):WaitForObject()
 local isVisible = false
-
+local timeInPhase = 0
 
 function GetWeapon(player)
     for _, v in ipairs(player:GetEquipment()) do
@@ -12,22 +12,30 @@ function GetWeapon(player)
     end
 end
 
-function Tick()
+function Tick(delta)
     local currentWeapon = GetWeapon(LOCAL_PLAYER)
     if currentWeapon then
         local reloadAbility = currentWeapon.clientUserData.RELOAD_ABILITY
 
         if reloadAbility and reloadAbility:GetCurrentPhase() == AbilityPhase.CAST then
+            timeInPhase = timeInPhase + delta
             local timeRemaining = reloadAbility:GetPhaseTimeRemaining()
             local maxTime = reloadAbility.castPhaseSettings.duration
-            PROGRESS_BAR.progress = 1 - (timeRemaining / reloadAbility.castPhaseSettings.duration)
+
+            if LOCAL_PLAYER.clientUserData.reloadMultiplier then
+                maxTime = maxTime * LOCAL_PLAYER.clientUserData.reloadMultiplier
+            end
+
+            PROGRESS_BAR.progress = timeInPhase / maxTime
+
             if not isVisible then
-            PARENT_PANEL.visibility = Visibility.FORCE_ON
-            isVisible = true
+                PARENT_PANEL.visibility = Visibility.FORCE_ON
+                isVisible = true
             end
         elseif isVisible then
             PARENT_PANEL.visibility = Visibility.FORCE_OFF
             isVisible = false
+            timeInPhase = 0
         end
     end
 end
