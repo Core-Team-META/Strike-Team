8875f2bee671c4a6c4726aab3503bc54ec4df796
Brent Dunn <brent075@gmail.com>
2021-03-05 11:31:05 -0800

    Fix: New player listeners being connected on every respawn

--- Strike Team/Data/Scripts/Player Movement.lua
+++ Strike Team/Data/Scripts/Player Movement.lua
@@ -12,51 +12,60 @@ local DEFAULT_MOVEMENT_MODE = MovementControlMode.LOOK_RELATIVE
 
 local playerStances = {}
 local slidingTimers = {}
+local playerListners = {}
 
 local sprintboost = SPRINT_SPEED * 1.2
 
 function AddSprintBoost(player)
-    if not player.serverUserData.EquippedWeapon then return SPRINT_SPEED end
+    if not player.serverUserData.EquippedWeapon then
+        return SPRINT_SPEED
+    end
     if not player.serverUserData.EquippedWeapon:IsA("Weapon") and player.serverUserData.EquippedWeapon:IsA("Equipment") then
-
         return sprintboost
     end
     return SPRINT_SPEED
 end
 
 function WillAffectMovement(player)
-    if not player.serverUserData.EquippedWeapon then return false end
+    if not player.serverUserData.EquippedWeapon then
+        return false
+    end
     if not player.serverUserData.EquippedWeapon:IsA("Weapon") and player.serverUserData.EquippedWeapon:IsA("Equipment") then
-
         return false
     end
     return true
 end
 
-
 function Tick(dt)
-    if _G["MovementCanControl"] == false then return end
+    if _G["MovementCanControl"] == false then
+        return
+    end
     for _, player in pairs(Game.GetPlayers()) do
         if player.isCrouching ~= player.serverUserData.playerStatus["Crouching"] then
             player.serverUserData.playerStatus["Crouching"] = player.isCrouching
             UpdatePlayerSliding(player)
-        end 
+        end
     end
 
     for player, slidingTimer in pairs(slidingTimers) do
-        if Object.IsValid(player) then 
-            if player.serverUserData.playerStatus and player.serverUserData.playerStatus["Sliding"] and time() >= slidingTimer then
-            player.serverUserData.playerStatus["Sliding"] = false
-            UpdatePlayerSliding(player)
-            UpdatePlayerAiming(player)
-            slidingTimers[player] = nil
+        if Object.IsValid(player) then
+            if
+                player.serverUserData.playerStatus and player.serverUserData.playerStatus["Sliding"] and
+                    time() >= slidingTimer
+             then
+                player.serverUserData.playerStatus["Sliding"] = false
+                UpdatePlayerSliding(player)
+                UpdatePlayerAiming(player)
+                slidingTimers[player] = nil
             end
         end
     end
 end
 
 function OnBindingPressed(player, key)
-    if _G["MovementCanControl"] == false then return end
+    if _G["MovementCanControl"] == false then
+        return
+    end
     if key == "ability_feet" then -- Shift
         player.serverUserData.playerStatus["ShiftDown"] = true
         UpdatePlayerSprinting(player)
@@ -70,7 +79,9 @@ function OnBindingPressed(player, key)
 end
 
 function OnBindingReleased(player, key)
-    if _G["MovementCanControl"] == false then return end
+    if _G["MovementCanControl"] == false then
+        return
+    end
     if key == "ability_feet" then -- Shift
         player.serverUserData.playerStatus["ShiftDown"] = false
         UpdatePlayerSprinting(player)
@@ -84,10 +95,12 @@ function OnBindingReleased(player, key)
 end
 
 function UpdatePlayerSprinting(player)
-   if _G["MovementCanControl"] == false then return end
+    if _G["MovementCanControl"] == false then
+        return
+    end
 
     if player.serverUserData.playerStatus["ShiftDown"] then
-        if  not player.serverUserData.playerStatus["Aiming"] and not player.serverUserData.playerStatus["Sliding"] then
+        if not player.serverUserData.playerStatus["Aiming"] and not player.serverUserData.playerStatus["Sliding"] then
             player.serverUserData.playerStatus["Sprinting"] = true
             player.maxWalkSpeed = AddSprintBoost(player)
             player.groundFriction = DEFAULT_FRICTION
@@ -107,12 +120,12 @@ function UpdatePlayerSprinting(player)
 end
 
 function UpdatePlayerAiming(player)
-    local AffectAiming =  WillAffectMovement(player)
+    local AffectAiming = WillAffectMovement(player)
     if player.serverUserData.playerStatus["RMBDown"] or player.serverUserData.playerStatus["LMBDown"] then
         player.serverUserData.playerStatus["Aiming"] = true
         player.serverUserData.playerStatus["Sprinting"] = false
-        if  not player.serverUserData.playerStatus["Sliding"] then
-            if  player.serverUserData.playerStatus["RMBDown"] then
+        if not player.serverUserData.playerStatus["Sliding"] then
+            if player.serverUserData.playerStatus["RMBDown"] then
                 player.maxWalkSpeed = SCOPE_SPEED
             elseif player.serverUserData.playerStatus["LMBDown"] and AffectAiming then
                 player.maxWalkSpeed = RUN_SPEED
@@ -130,7 +143,10 @@ function UpdatePlayerAiming(player)
 end
 
 function UpdatePlayerSliding(player)
-    if player.serverUserData.playerStatus["Crouching"] and player.serverUserData.playerStatus["Sprinting"] and not player.serverUserData.playerStatus["Sliding"] then
+    if
+        player.serverUserData.playerStatus["Crouching"] and player.serverUserData.playerStatus["Sprinting"] and
+            not player.serverUserData.playerStatus["Sliding"]
+     then
         player.serverUserData.playerStatus["Sprinting"] = false
         player.serverUserData.playerStatus["Sliding"] = true
         player.groundFriction = SLIDING_FRICTION
@@ -153,12 +169,15 @@ function UpdatePlayerSliding(player)
     UpdatePlayerStance(player)
 end
 
+function ConnectListeners(player)
+    playerListners[player.id]["bindingPressed"] = player.bindingPressedEvent:Connect(OnBindingPressed)
+    playerListners[player.id]["bindingReleased"] = player.bindingReleasedEvent:Connect(OnBindingReleased)
+end
+
 function SetUp(player)
     player.maxWalkSpeed = RUN_SPEED
     player.groundFriction = DEFAULT_FRICTION
     player.brakingDecelerationWalking = DEFAULT_BRAKING
-    player.bindingPressedEvent:Connect(OnBindingPressed)
-    player.bindingReleasedEvent:Connect(OnBindingReleased)
     player.serverUserData.playerStatus = {
         ["Crouching"] = false,
         ["Sliding"] = false,
@@ -172,8 +191,10 @@ function SetUp(player)
 end
 
 function PlayerJoined(player)
-    player.respawnedEvent:Connect(SetUp)
+    playerListners[player.id] = {}
+    playerListners[player.id]["Respawn"] = player.respawnedEvent:Connect(SetUp)
     SetUp(player)
+    ConnectListeners(player)
 end
 
 function SetDefaultWeaponStances(player)
@@ -185,8 +206,12 @@ end
 
 function UpdatePlayerStance(player)
     --print(playerStances[player]["Sprinting"], playerStances[player]["Aiming"])
-    if not player.serverUserData.playerStatus then return end
-    if player.serverUserData.playerStatus["Crouching"] then return end
+    if not player.serverUserData.playerStatus then
+        return
+    end
+    if player.serverUserData.playerStatus["Crouching"] then
+        return
+    end
 
     if player.serverUserData.playerStatus["Sprinting"] then
         player.animationStance = playerStances[player]["Sprinting"]
@@ -196,12 +221,22 @@ function UpdatePlayerStance(player)
 end
 
 function PlayerLeft(player)
+    if playerListners[player.id] then
+        for _, listner in pairs(playerListners[player.id]) do
+            if listner.isConnected then
+                listner:Disconnect()
+            end
+        end
+        playerListners[player.id] = nil
+    end
     player.serverUserData.playerStatus = nil
     playerStances[player] = nil
 end
 
 function OnEquipWeapon(owner, weapon)
-    if not Object.IsValid( weapon)  then return end
+    if not Object.IsValid(weapon) then
+        return
+    end
     SetDefaultWeaponStances(owner)
     local sprintingStance = weapon:GetCustomProperty("SprintingStance")
     local aimingStance = weapon:GetCustomProperty("AimActiveStance")
@@ -212,14 +247,15 @@ function OnEquipWeapon(owner, weapon)
     end
     playerStances[owner]["Sprinting"] = sprintingStance or "2hand_rifle_stance"
     playerStances[owner]["Aiming"] = aimingStance or "2hand_rifle_aim_shoulder"
-    
+
     Task.Wait()
-    if not Object.IsValid(owner) then return end
-    
+    if not Object.IsValid(owner) then
+        return
+    end
+
     UpdatePlayerStance(owner)
     UpdatePlayerSprinting(owner)
 end
-
 
 Game.playerJoinedEvent:Connect(PlayerJoined)
 Game.playerLeftEvent:Connect(PlayerLeft)
