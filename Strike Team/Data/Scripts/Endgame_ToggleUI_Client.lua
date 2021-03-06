3ffcd33e69712a26fb2ce2468a06d0c1958eebdd
Brent Dunn <brent075@gmail.com>
2021-03-05 18:46:11 -0800

    Maint: Removed hardcoded 45 second state timer

--- Strike Team/Data/Scripts/Endgame_ToggleUI_Client.lua
+++ Strike Team/Data/Scripts/Endgame_ToggleUI_Client.lua
@@ -100,7 +100,7 @@ function OnGameStateChanged(oldState, newState, hasDuration, time)
     elseif newState == ABGS.GAME_STATE_ROUND_END and oldState ~= ABGS.GAME_STATE_ROUND_END then
         World.SpawnAsset(RoundEndUIDisplayTemplate)
         PLAYER_PANELS.visibility = Visibility.FORCE_ON
-        stateTime = ABGS.GetTimeRemainingInState() or 45
+        stateTime = ABGS.GetTimeRemainingInState()
         ClearActivePanels()
         Task.Wait(5)
 
