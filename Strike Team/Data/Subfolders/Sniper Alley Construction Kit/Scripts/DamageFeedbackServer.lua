064874e9636bf85e54181a2a63969fca650ee09a
blaking707 <46813060+blaking707@users.noreply.github.com>
2021-03-07 13:48:20 +1100

    Remove queue from Damage Feedback
    (cherry picked from commit e645452cd62660c450ab49744ec82ac140a7e2ce)

--- Strike Team/Data/Subfolders/Sniper Alley Construction Kit/Scripts/DamageFeedbackServer.lua
+++ Strike Team/Data/Subfolders/Sniper Alley Construction Kit/Scripts/DamageFeedbackServer.lua
@@ -14,76 +14,12 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --]]
-local ReliableEvents = require(script:GetCustomProperty("ReliableEvents"))
-local QueueTable = {}
-local Queue = {}
-Queue.__index = Queue
-
-function Queue.New(time,source,damaged,amount)
-    local o = setmetatable({}, Queue)
-    o.time = time
-    o.source = source
-    o.damaged = damaged
-    o.amount = amount
-    return o
-end
-
-function Queue:IsSource(val)
-    return val == self.source
-end
-
-function Queue:IsDamaged(val)
-    return val == self.damaged
-end
-
-function Queue:AddDamage(damage)
-    self.amount = self.amount + damage
-end
-
-function Queue:ReturnTime()
-    return os.clock() - self.time 
-end
-
-function Queue:Broadcast()
-    Events.BroadcastToAllPlayers ("PlayerDamage_Internal", self.amount, self.damaged, self.source)
-end
-
-function AddToQueueTable(player, damage )
-    table.insert( QueueTable, Queue.New(os.clock(),damage.sourcePlayer,player,0))
-end
-
-function CheckQueueTable(player,damage)
-    if player == damage.sourcePlayer  then return end
-    for _, Q in ipairs(QueueTable) do
-        if Q:IsSource(damage.sourcePlayer) and Q:IsDamaged(player) then 
-            Q:AddDamage(damage.amount)
-            return 
-        end
-    end
-    Events.BroadcastToAllPlayers("PlayerDamage_Internal", damage.amount, player, damage.sourcePlayer)
-    AddToQueueTable(player, damage )
-end
-
-function ClearQueues()
-    while #QueueTable > 0 do
-        for i, Q in ipairs(QueueTable) do
-            if Q:ReturnTime() >= 0.1 then
-                if Q.amount > 0 then 
-                    Q:Broadcast()
-                end
-                QueueTable[i] = nil
-            end
-        end
-        Task.Wait()
-    end
-end
 
 -- nil OnPlayerDied(Player, Damage)
 -- Fires an event for the client to display fly up text when hit the enemy player
 function OnPlayerDamaged(player, damage)
     if damage.sourcePlayer then
-        CheckQueueTable(player,damage)
-        ClearQueues()
+        Events.BroadcastToAllPlayers("PlayerDamage_Internal", damage.amount, player, damage.sourcePlayer)
     end
 end
 
