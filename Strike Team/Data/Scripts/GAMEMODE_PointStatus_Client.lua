d7094c4d306dc7fd2b501353838554db4c9d54c2
Brent Dunn <brent075@gmail.com>
2021-03-16 19:32:05 -0700

    Fix: Support Point UI showing for all players, if one player is in the support point

--- Strike Team/Data/Scripts/GAMEMODE_PointStatus_Client.lua
+++ Strike Team/Data/Scripts/GAMEMODE_PointStatus_Client.lua
@@ -2,31 +2,32 @@ local LOCAL_PLAYER = Game.GetLocalPlayer()
 local TRIGGER = script:GetCustomProperty("Trigger"):WaitForObject()
 local SUPPORT_TIGGER = script:GetCustomProperty("SupportTrigger"):WaitForObject()
 
-
 function OnBeginOverlap(trigger, object)
-    local panel = LOCAL_PLAYER.clientUserData.pointStatusUI
-    panel.visibility = Visibility.FORCE_ON
-    local info = panel:GetCustomProperty("POINT_STATUS"):WaitForObject()
-    if trigger == TRIGGER then
-        info.text = "On Capture"
-    end
-    if trigger == SUPPORT_TIGGER then
-        info.text = "Support Capture"
+    if object == LOCAL_PLAYER then
+        local panel = LOCAL_PLAYER.clientUserData.pointStatusUI
+        panel.visibility = Visibility.FORCE_ON
+        local info = panel:GetCustomProperty("POINT_STATUS"):WaitForObject()
+        if trigger == TRIGGER then
+            info.text = "On Capture"
+        end
+        if trigger == SUPPORT_TIGGER then
+            info.text = "Support Capture"
+        end
     end
 end
-
 
 function OnEndOverlap(trigger, object)
-    local panel = LOCAL_PLAYER.clientUserData.pointStatusUI
-    local info = panel:GetCustomProperty("POINT_STATUS"):WaitForObject()
-    if trigger == TRIGGER then
-        info.text = "Support Capture"
-    end
-    if trigger == SUPPORT_TIGGER then
-        panel.visibility = Visibility.FORCE_OFF
+    if object == LOCAL_PLAYER then
+        local panel = LOCAL_PLAYER.clientUserData.pointStatusUI
+        local info = panel:GetCustomProperty("POINT_STATUS"):WaitForObject()
+        if trigger == TRIGGER then
+            info.text = "Support Capture"
+        end
+        if trigger == SUPPORT_TIGGER then
+            panel.visibility = Visibility.FORCE_OFF
+        end
     end
 end
-
 
 -- handler params: Trigger_, Object_
 TRIGGER.beginOverlapEvent:Connect(OnBeginOverlap)
