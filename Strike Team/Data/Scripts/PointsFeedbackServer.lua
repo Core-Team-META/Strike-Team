2a20d1d6033b1198674d911b75740b29b4ab0cf2
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2021-03-19 21:36:29 -0700

    Added first version of points feed system.

---
+++ Strike Team/Data/Scripts/PointsFeedbackServer.lua
@@ -0,0 +1,26 @@
+while not _G["DataBase"] do Task.Wait() end
+local database = _G["DataBase"]
+
+function OnResourceChanged(player, resourceName, resourceValue)
+
+	if resourceName == "Score Total" or resourceValue == 0 then 
+	
+		return
+		
+	end
+
+	--print("Resource Changed " .. resourceName)
+
+	player:AddResource("Score Total", resourceValue)
+
+end
+
+function ConnectPlayer(player)
+
+	Task.Wait(1)
+
+	player.resourceChangedEvent:Connect(OnResourceChanged)
+
+end
+
+Game.playerJoinedEvent:Connect(ConnectPlayer)
