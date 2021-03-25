c77b4bed44a27cbaf5ce4f5c3c7c3a628c6676db
junster09 <ajr2z2025@gmail.com>
2021-03-24 17:17:10 -0700

    cleanup script before easeUI

--- Strike Team/Data/Scripts/BasicDoorControllerServer.asset.pbt
+++
@@ -1,8 +0,0 @@
-Assets {
-  Id: 1031736639489708352
-  Name: "BasicDoorControllerServer"
-  PlatformAssetType: 3
-  TextAsset {
-  }
-  SerializationVersion: 78
-}
--- Strike Team/Data/Scripts/Drawtest.lua
+++
@@ -1,7 +0,0 @@
-local player = Game.GetLocalPlayer()
-while true do
-
-    local pos =  (player:GetViewWorldRotation() * Vector3.FORWARD)
-    CoreDebug.DrawLine(player:GetWorldPosition(), player:GetWorldPosition()+(pos*100))
-    Task.Wait()
-end
