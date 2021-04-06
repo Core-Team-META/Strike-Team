3558d53d13e15326428ddac2c50eb7392de89267
Rolok <imrolok@yahoo.com>
2021-04-06 13:59:51 -0700

    updated news

--- Strike Team/Data/Scripts/RichText_EXAMPLE.lua
+++ Strike Team/Data/Scripts/RichText_EXAMPLE.lua
@@ -24,20 +24,22 @@ Text that has an image (<image sword>) which I think is very nice!</font>
 
 
 local text2 = [[
+<size 25><font play_bold>4/06/2021</font></size>
+<image gradient -1 3 #FF3300>
+<image unlock 50 50><panel -1 #FF330022>Unlock the new HLR (High Latency Rifle) and Disco Ball grenade (guaranteed to make you dance)</panel>
+<image unlock 50 50><panel -1 #FF330022>St. Patricks Day skins are no longer available, but Spring skins are now available -- and free!  Pick them up in the Loadout</panel>
+
 <size 25><font play_bold>3/25/2021</font></size>
 <image gradient -1 3 #FF3300>
 <image trophy 50 50><panel -1 #FF330022>Guns n' Glory Tournament officially ended today. Thanks for all who played, we'll be updating our scoring for the next tournament</panel>
 
 <size 25><font play_bold>3/20/2021</font></size>
 <image gradient -1 3 #FF3300>
-<image cash 50 50><panel -1 #FF330022>Guns n' Glory Tournament officially ended today. Thanks for all who played, we'll be updating our scoring for the next tournament</panel>
 <image trophy 50 50><panel -1 #FF330022>Guns n' Glory Tournament continues - hit K key to see the tournament leaderboards</panel>
-<image unlock 50 50><panel -1 #FF330022>Win with or against the Devs and get cool skins! Watch out for Basilisk, Vitriol08, Buckmonster, Bigglebuns, Rolok, Blaking707, Morticai, AwkwardGameDev, Mucusinator, WitcherSilver, Aggripina, Coderz, AJ, Divided, Estlogic, Griffin, Scav, Standardcombo, Ooccoo, Mehaji, Melamory, and more to come</panel>
+<image unlock 50 50><panel -1 #FF330022>Win with or against the Devs and get cool skins! Watch out for Basilisk, Vitriol08, Buckmonster, Bigglebuns, Rolok, Blaking707, Morticai, AwkwardGameDev, Mucusinator, WitcherSilver, Aggripina, Coderz, AJ, Divided, Estly</panel>
 
 <size 25><font play_bold>3/18/2021</font></size>
 <image gradient -1 3 #FF3300>
-<image trophy 50 50><panel -1 #FF330022><image trophy 50 50><panel -1 #FFFFFF22>Guns n' Glory Tournament officially ended today. Thanks for all who played, we'll be updating our scoring for the next tournament</panel>
-</panel>
 <image chest 50 50><panel -1 #FF330022>Guns n' Glory Tournament Begins Today! See details at https://gunsnglory.coregames.com/</panel>
 
 ]]
