0d16ea87e63da1cb4904f4d7f9502ca3f5c3f6c0
Rolok <imrolok@yahoo.com>
2021-04-28 11:50:48 -0700

    removed modelGender related code

--- Strike Team/Data/Scripts/FallDamageClient.lua
+++ Strike Team/Data/Scripts/FallDamageClient.lua
@@ -23,14 +23,7 @@ local FALL_DAMAGE_SOUND_TEMPLATE = FALL_DAMAGE_SOUND_MALE
 
 -- nil OnFallDamage(Player)
 -- Handles a player damage event and plays the corresponding sound
-function OnFallDamage(player, modelGender)
-
---[[ 	if (modelGender ~= nil and modelGender == "Female") then
-		FALL_DAMAGE_SOUND_TEMPLATE = FALL_DAMAGE_SOUND_FEMALE
-	else
-		FALL_DAMAGE_SOUND_TEMPLATE = FALL_DAMAGE_SOUND_MALE
-	end
- ]]
+function OnFallDamage(player)
  	World.SpawnAsset(FALL_DAMAGE_SOUND_TEMPLATE, {position = player:GetWorldPosition()})
 end
 
--- Strike Team/Data/Scripts/FallDamageServer.lua
+++ Strike Team/Data/Scripts/FallDamageServer.lua
@@ -24,7 +24,6 @@ local LETHAL_SPEED = TEMPLATE_ROOT:GetCustomProperty("LethalSpeed")
 local HEAR_OTHER_PLAYERS_DAMAGE_SOUNDS = TEMPLATE_ROOT:GetCustomProperty("HearOtherPlayersDamageSounds")
 
 local SPHERE = script:GetCustomProperty("Sphere")
-local modelGender = "Male"
 
 -- Check user properties
 if MAXIMUM_SAFE_SPEED <= 0.0 then
@@ -37,38 +36,6 @@ if LETHAL_SPEED < MAXIMUM_SAFE_SPEED then
 	LETHAL_SPEED = MAXIMUM_SAFE_SPEED
 end
 
-function AttachPoints(player)
-
-    local Left = World.SpawnAsset(SPHERE)
-    local Right = World.SpawnAsset(SPHERE)
-
-    Left.collision = Collision.FORCE_OFF
-    Right.collision = Collision.FORCE_OFF
-    Left.visibility = Visibility.FORCE_OFF
-    Right.visibility = Visibility.FORCE_OFF
-
-	Task.Wait(1)
-
-	Left:AttachToPlayer(player, "left_clavicle")
-    Right:AttachToPlayer(player, "right_clavicle")
-
-	if((Left:GetWorldPosition() - Right:GetWorldPosition()).size <= 6) then
-        modelGender = "Female" -- Female
-    else
-        modelGender = "Male" -- Male
-    end
-
-	if (Object.IsValid(Left)) then
-		Left:Detach()
-		Left:Destroy()
-	end
-	if (Object.IsValid(Right)) then
-		Right:Detach()
-		Right:Destroy()
-	end
-    
-end
-
 -- Variables
 local previousFallingSpeeds = {}		-- Player -> float
 local previousGroundedStates = {}		-- Player -> bool
@@ -76,7 +43,7 @@ local previousGroundedStates = {}		-- Player -> bool
 -- nil OnPlayerJoined(Player)
 -- Sets up data for a new player
 function OnPlayerJoined(player)
-	AttachPoints(player)
+--	AttachPoints(player)
 	previousFallingSpeeds[player] = 0.0
 	previousGroundedStates[player] = true
 end
@@ -113,9 +80,9 @@ function Tick(deltaTime)
 
 				-- Send an event so the client can play sounds
 				if HEAR_OTHER_PLAYERS_DAMAGE_SOUNDS then
-					Events.BroadcastToAllPlayers("FallDamage", player, modelGender)
+					Events.BroadcastToAllPlayers("FallDamage", player)
 				else
-					Events.BroadcastToPlayer(player, "FallDamage", player, modelGender)
+					Events.BroadcastToPlayer(player, "FallDamage", player)
 				end
 			end
 		end
@@ -130,6 +97,6 @@ end
 Game.playerJoinedEvent:Connect(OnPlayerJoined)
 Game.playerLeftEvent:Connect(OnPlayerLeft)
 
-Events.Connect("UpdateSkin", AttachPoints)
+--Events.Connect("UpdateSkin", AttachPoints)
 
 
