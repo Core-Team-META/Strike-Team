a21695f90e4c8b56c3df3debd2281191b7685cb4
Rolok <imrolok@yahoo.com>
2021-06-18 12:55:53 -0700

    changed news to point to player discord

--- Strike Team/Data/Scripts/RichText_EXAMPLE.lua
+++ Strike Team/Data/Scripts/RichText_EXAMPLE.lua
@@ -24,30 +24,9 @@ Text that has an image (<image sword>) which I think is very nice!</font>
 
 
 local text2 = [[
-
-<size 25><font play_bold>6/11/2021</font></size>
 <image gradient -1 3 #FF3300>
-<panel -1 #FF330022>A new chapter for Strike Team has been released!  Strike Team: Sector 9.</panel>
-<panel -1 #FF330022>It's a cyberpunk theme with much more open sight lines.  Check it out!</panel>
-    
-
-<size 25><font play_bold>4/06/2021</font></size>
-<image gradient -1 3 #FF3300>
-<image unlock 50 50><panel -1 #FF330022>Unlock the new HLR (High Latency Rifle) and Disco Ball grenade (guaranteed to make you dance)</panel>
-<image unlock 50 50><panel -1 #FF330022>St. Patricks Day skins are no longer available, but Spring skins are now available -- and free!  Pick them up in the Loadout</panel>
-
-<size 25><font play_bold>3/25/2021</font></size>
-<image gradient -1 3 #FF3300>
-<image trophy 50 50><panel -1 #FF330022>Guns n' Glory Tournament officially ended today. Thanks for all who played, we'll be updating our scoring for the next tournament</panel>
-
-<size 25><font play_bold>3/20/2021</font></size>
-<image gradient -1 3 #FF3300>
-<image trophy 50 50><panel -1 #FF330022>Guns n' Glory Tournament continues - hit K key to see the tournament leaderboards</panel>
-<image unlock 50 50><panel -1 #FF330022>Win with or against the Devs and get cool skins! Watch out for Basilisk, Vitriol08, Buckmonster, Bigglebuns, Rolok, Blaking707, Morticai, AwkwardGameDev, Mucusinator, WitcherSilver, Aggripina, Coderz, AJ, Divided, Estly</panel>
-
-<size 25><font play_bold>3/18/2021</font></size>
-<image gradient -1 3 #FF3300>
-<image chest 50 50><panel -1 #FF330022>Guns n' Glory Tournament Begins Today! See details at https://gunsnglory.coregames.com/</panel>
+<panel -1 #FF330022>For the most recent news check out the Strike Team channel on the player discord</panel>
+<panel -1 #FF330022>https://discord.gg/Cp7VEsqg</panel>
 
 ]]
 
